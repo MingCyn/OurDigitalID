d26f73f432d3e3f24f02beb753ed8c417ca13509
y1iiiiiiii1 <jaimelai888@gmail.com>
2026-04-15 23:33:10 +0800

    scan button

--- app/home/Home.tsx
+++ app/home/Home.tsx
@@ -555,7 +555,7 @@ export default function HomeScreen() {
 
           <TouchableOpacity
             style={[styles.actionButton, { backgroundColor: "#F3E5F5" }]}
-            onPress={() => handleActionPress("/scan")}
+            onPress={() => handleActionPress("service/scan.tsx")}
           >
             <AppText
               size={12}
